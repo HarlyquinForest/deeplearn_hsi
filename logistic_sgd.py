de37005f75e18b972a38bb76d3d6bcf3e0213955
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-07-28 18:20:42 +0430

    import pickle + replace / with // + replace xrange with range + replace time.clock() with time.pref_counter() + python3 syntax

--- logistic_sgd.py
+++ logistic_sgd.py
@@ -34,7 +34,7 @@ References:
 """
 __docformat__ = 'restructedtext en'
 
-import cPickle
+import pickle
 import gzip
 import os
 import sys
@@ -191,18 +191,18 @@ def load_data(dataset):
             dataset = new_path
 
     if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
-        import urllib
+        import urllib.request, urllib.parse, urllib.error
         origin = (
             'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
         )
-        print 'Downloading data from %s' % origin
-        urllib.urlretrieve(origin, dataset)
+        print(('Downloading data from %s' % origin))
+        urllib.request.urlretrieve(origin, dataset)
 
-    print '... loading data'
+    print ('... loading data')
 
     # Load the dataset
     f = gzip.open(dataset, 'rb')
-    train_set, valid_set, test_set = cPickle.load(f)
+    train_set, valid_set, test_set = pickle.load(f)
     f.close()
     #train_set, valid_set, test_set format: tuple(input, target)
     #input is an numpy.ndarray of 2 dimensions (a matrix)
@@ -273,14 +273,14 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
     test_set_x, test_set_y = datasets[2]
 
     # compute number of minibatches for training, validation and testing
-    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
-    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] / batch_size
-    n_test_batches = test_set_x.get_value(borrow=True).shape[0] / batch_size
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] // batch_size
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] // batch_size
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0] // batch_size
 
     ######################
     # BUILD ACTUAL MODEL #
     ######################
-    print '... building the model'
+    print( '... building the model')
 
     # allocate symbolic variables for the data
     index = T.lscalar()  # index to a [mini]batch
@@ -345,7 +345,7 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
     ###############
     # TRAIN MODEL #
     ###############
-    print '... training the model'
+    print('... training the model')
     # early-stopping parameters
     patience = 5000  # look as this many examples regardless
     patience_increase = 2  # wait this much longer when a new best is
@@ -360,13 +360,13 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
 
     best_validation_loss = numpy.inf
     test_score = 0.
-    start_time = time.clock()
+    start_time = time.perf_counter()
 
     done_looping = False
     epoch = 0
     while (epoch < n_epochs) and (not done_looping):
         epoch = epoch + 1
-        for minibatch_index in xrange(n_train_batches):
+        for minibatch_index in range(n_train_batches):
 
             minibatch_avg_cost = train_model(minibatch_index)
             # iteration number
@@ -375,10 +375,10 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
             if (iter + 1) % validation_frequency == 0:
                 # compute zero-one loss on validation set
                 validation_losses = [validate_model(i)
-                                     for i in xrange(n_valid_batches)]
+                                     for i in range(n_valid_batches)]
                 this_validation_loss = numpy.mean(validation_losses)
 
-                print(
+                print((
                     'epoch %i, minibatch %i/%i, validation error %f %%' %
                     (
                         epoch,
@@ -386,7 +386,7 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
                         n_train_batches,
                         this_validation_loss * 100.
                     )
-                )
+                ))
 
                 # if we got the best validation score until now
                 if this_validation_loss < best_validation_loss:
@@ -399,10 +399,10 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
                     # test it on the test set
 
                     test_losses = [test_model(i)
-                                   for i in xrange(n_test_batches)]
+                                   for i in range(n_test_batches)]
                     test_score = numpy.mean(test_losses)
 
-                    print(
+                    print((
                         (
                             '     epoch %i, minibatch %i/%i, test error of'
                             ' best model %f %%'
@@ -413,25 +413,25 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
                             n_train_batches,
                             test_score * 100.
                         )
-                    )
+                    ))
 
             if patience <= iter:
                 done_looping = True
                 break
 
-    end_time = time.clock()
-    print(
+    end_time = time.perf_counter()
+    print((
         (
             'Optimization complete with best validation score of %f %%,'
             'with test performance %f %%'
         )
         % (best_validation_loss * 100., test_score * 100.)
-    )
-    print 'The code run for %d epochs, with %f epochs/sec' % (
-        epoch, 1. * epoch / (end_time - start_time))
-    print >> sys.stderr, ('The code for file ' +
+    ))
+    print('The code run for %d epochs, with %f epochs/sec' % (
+        epoch, 1. * epoch / (end_time - start_time)))
+    print(('The code for file ' +
                           os.path.split(__file__)[1] +
-                          ' ran for %.1fs' % ((end_time - start_time)))
+                          ' ran for %.1fs' % ((end_time - start_time))), file=sys.stderr)
 
 if __name__ == '__main__':
     sgd_optimization_mnist()
