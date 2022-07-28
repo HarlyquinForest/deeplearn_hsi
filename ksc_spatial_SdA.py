695bb3cefaa470200565e758e9622ca9e286300e
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-07-28 18:18:56 +0430

    import imageio + replace scipy.misc.imsave with imageio.imwrite + replace time.clock() with time.pref_counter() + replace xrange with range + replace with / with // + python3 synatx

--- ksc_spatial_SdA.py
+++ ksc_spatial_SdA.py
@@ -5,6 +5,8 @@ __version__ = "1.0"
 import os
 import sys
 import time
+
+import imageio
 import scipy
 import scipy.io as sio
 import numpy
@@ -35,9 +37,9 @@ cmap = numpy.asarray( [[0, 0, 0],
 
 
 # load .mat files
-print '... loading data'
-hsi_file = u'/home/hantek/data/hsi_data/kennedy/KSC.mat'
-gnd_file = u'/home/hantek/data/hsi_data/kennedy/KSC_gt.mat'
+print('... loading data')
+hsi_file = 'datasets/KSC.mat'
+gnd_file = 'datasets/KSC_gt.mat'
 data = sio.loadmat(hsi_file)
 img = scale_to_unit_interval(data['KSC'].astype(theano.config.floatX))
 width = img.shape[0]
@@ -48,12 +50,12 @@ gnd_img = data['KSC_gt']
 gnd_img = gnd_img.astype(numpy.int32)
 
 # PCA transform image, condense its spectral size.
-print '... extracting data points'
+print('... extracting data points')
 _, datasets, _, _ = \
     prepare_data(hsi_img=img, gnd_img=gnd_img, merge=False, 
                  window_size=7, n_principle=4, batch_size=100)
 
-print '... training'
+print('... training')
 # begin SdA
 finetune_lr=0.008
 pretraining_epochs=2000
@@ -62,21 +64,21 @@ training_epochs=200000
 batch_size=50
 hidden_layers_sizes=[310, 310, 310, 310, 310]
 corruption_levels = [0., 0., 0., 0., 0.]
-print 'finetuning learning rate=', finetune_lr
-print 'pretraining learning rate=', pretrain_lr
-print 'pretraining epoches=', pretraining_epochs
-print 'fine tuning epoches=', training_epochs
-print 'batch size=', batch_size
-print 'hidden layers sizes=', hidden_layers_sizes
-print 'corruption levels=', corruption_levels
+print('finetuning learning rate=', finetune_lr)
+print('pretraining learning rate=', pretrain_lr)
+print('pretraining epoches=', pretraining_epochs)
+print('fine tuning epoches=', training_epochs)
+print('batch size=', batch_size)
+print('hidden layers sizes=', hidden_layers_sizes)
+print('corruption levels=', corruption_levels)
 
 # compute number of minibatches for training, validation and testing
 n_train_batches = datasets[0][0].get_value(borrow=True).shape[0]
-n_train_batches /= batch_size
+n_train_batches //= batch_size
 
 # numpy random generator
 numpy_rng = numpy.random.RandomState(89677)
-print '... building the model'
+print('... building the model')
 # construct the stacked denoising autoencoder class
 sda = SdA(numpy_rng=numpy_rng, n_ins=datasets[0][0].get_value(borrow=True).shape[1],
           hidden_layers_sizes=hidden_layers_sizes,
@@ -86,43 +88,43 @@ sda = SdA(numpy_rng=numpy_rng, n_ins=datasets[0][0].get_value(borrow=True).shape
                            # PRETRAINING THE MODEL #
                            #########################
 
-print '... getting the pretraining functions'
+print('... getting the pretraining functions')
 pretraining_fns = sda.pretraining_functions(train_set_x=datasets[0][0],
                                             batch_size=batch_size)
 
-print '... pre-training the model'
-start_time = time.clock()
+print('... pre-training the model')
+start_time = time.perf_counter()
 ## Pre-train layer-wise
-for i in xrange(sda.n_layers):
+for i in range(sda.n_layers):
     # go through pretraining epochs
-    for epoch in xrange(pretraining_epochs):
+    for epoch in range(pretraining_epochs):
         # go through the training set
         c = []
-        for batch_index in xrange(n_train_batches):
+        for batch_index in range(n_train_batches):
             c.append(pretraining_fns[i](index=batch_index,
                      corruption=corruption_levels[i],
                      lr=pretrain_lr))
         
-        print 'Pre-training layer %i, epoch %d, cost ' % (i, epoch),
-        print numpy.mean(c)
+        print('Pre-training layer %i, epoch %d, cost ' % (i, epoch), end=' ')
+        print(numpy.mean(c))
 
-end_time = time.clock()
+end_time = time.perf_counter()
 
-print ('The pretraining code for file ' +
+print(('The pretraining code for file ' +
        os.path.split(__file__)[1] +
-       ' ran for %.2fm' % ((end_time - start_time) / 60.))
+       ' ran for %.2fm' % ((end_time - start_time) / 60.)))
 
 ################################################################################
                             # FINETUNING THE MODEL #
                             ########################
 
 # get the training, validation and testing function for the model
-print '... getting the finetuning functions'
+print('... getting the finetuning functions')
 train_fn, validate_model, test_model = sda.build_finetune_functions(
     datasets=datasets, batch_size=batch_size,
     learning_rate=finetune_lr)
 
-print '... finetunning the model'
+print('... finetunning the model')
 validation_frequency = 1000 * n_train_batches
                         # go through this many
                         # minibatche before checking the network
@@ -132,21 +134,21 @@ validation_frequency = 1000 * n_train_batches
 best_params = None
 best_validation_loss = numpy.inf
 test_score = 0.
-start_time = time.clock()
+start_time = time.perf_counter()
 
 epoch = 0
 while (epoch < training_epochs):
     epoch = epoch + 1
-    for minibatch_index in xrange(n_train_batches):
+    for minibatch_index in range(n_train_batches):
         minibatch_avg_cost = train_fn(minibatch_index)
         iter = (epoch - 1) * n_train_batches + minibatch_index
 
         if (iter + 1) % validation_frequency == 0:
             validation_losses = validate_model()
             this_validation_loss = numpy.mean(validation_losses)
-            print('epoch %i, minibatch %i/%i, validation error %f %%' %
+            print(('epoch %i, minibatch %i/%i, validation error %f %%' %
                   (epoch, minibatch_index + 1, n_train_batches,
-                   this_validation_loss * 100.))
+                   this_validation_loss * 100.)))
 
             # if we got the best validation score until now
             if this_validation_loss < best_validation_loss:
@@ -157,28 +159,28 @@ while (epoch < training_epochs):
                 # test it on the test set
                 test_losses = test_model()
                 test_score = numpy.mean(test_losses)
-                print(('     epoch %i, minibatch %i/%i, test error of '
+                print((('     epoch %i, minibatch %i/%i, test error of '
                        'best model %f %%') %
                       (epoch, minibatch_index + 1, n_train_batches,
-                       test_score * 100.))
+                       test_score * 100.)))
 
-end_time = time.clock()
-print(('Optimization complete with best validation score of %f %%,'
+end_time = time.perf_counter()
+print((('Optimization complete with best validation score of %f %%,'
        'with test performance %f %%') %
-      (best_validation_loss * 100., test_score * 100.))
-print ('The training code for file ' +
+      (best_validation_loss * 100., test_score * 100.)))
+print(('The training code for file ' +
        os.path.split(__file__)[1] +
-       ' ran for %.2fm' % ((end_time - start_time) / 60.))
+       ' ran for %.2fm' % ((end_time - start_time) / 60.)))
 
 
 filename = 'ksc_l1sda_pt%d_ft%d_lrp%.4f_f%.4f_bs%d_pca%d' % \
            (pretraining_epochs, training_epochs, pretrain_lr, finetune_lr, 
             batch_size, 4) 
 
-print '... saving parameters'
+print('... saving parameters')
 sda.save_params(filename + '_params.pkl')
 
-print '... classifying test set with learnt model:'
+print('... classifying test set with learnt model:')
 pred_func = theano.function(inputs=[sda.x], outputs=sda.logLayer.y_pred)
 pred_test = pred_func(datasets[2][0].get_value(borrow=True))
 true_test = datasets[2][1].get_value(borrow=True)
@@ -186,30 +188,30 @@ true_valid = datasets[1][1].get_value(borrow=True)
 true_train = datasets[0][1].get_value(borrow=True)
 result_analysis(pred_test, true_train, true_valid, true_test)
 
-print '... classifying the whole image with learnt model:'
-print '...... extracting data'
+print('... classifying the whole image with learnt model:')
+print('...... extracting data')
 window_size = 7
 _, data_spatial, _, _ = \
     T_pca_constructor(hsi_img=img, gnd_img=gnd_img, n_principle=4, 
                       window_size=window_size,  flag='unsupervised')
     
-start_time = time.clock()
-print '...... begin '
+start_time = time.perf_counter()
+print('...... begin ')
 y = pred_func(data_spatial) + 1
-print '...... done '
-end_time = time.clock()
-print 'finished, running time:%fs' % (end_time - start_time)
+print('...... done ')
+end_time = time.perf_counter()
+print('finished, running time:%fs' % (end_time - start_time))
 y_rgb = cmap[y, :]
-margin = (window_size / 2) * 2  # floor it to a multiple of 2
+margin = (window_size // 2) * 2  # floor it to a multiple of 2
 y_image = y_rgb.reshape(width - margin, height - margin, 3)
-scipy.misc.imsave(filename + '_wholeimg.png' , y_image)
+imageio.imwrite(filename + '_wholeimg.png' , y_image)
 
-print 'Saving classification results'
+print('Saving classification results')
 sio.savemat(filename + 'wholeimg.mat', 
             {'y': y.reshape(width - margin, height - margin)})
 
 ############################################################################
-print '... performing Student\'s t-test'
+print('... performing Student\'s t-test')
 best_c = 10000.
 best_g = 10.
 svm_classifier = svm.SVC(C=best_c, gamma=best_g, kernel='rbf')
@@ -221,27 +223,27 @@ data = [numpy.vstack((datasets[1][0].get_value(),
                       datasets[2][1].get_value()))]
 numpy_rng = numpy.random.RandomState(89677)
 num_test = 100
-print 'Total number of tests: %d' % num_test
+print('Total number of tests: %d' % num_test)
 k_sae = []
 k_svm = []
-for i in xrange(num_test):
+for i in range(num_test):
     [_, _], [_, _], [test_x, test_y], _ = \
     train_valid_test(data, ratio=[0, 1, 1], batch_size=1, 
-                     random_state=numpy_rng.random_integers(1e10))
+                     random_state=numpy_rng.randint(2**30))
     test_y = test_y + 1 # fix the label scale problem
     pred_y = pred_func(test_x)
     cm = confusion_matrix(test_y, pred_y)
-    pr_a = cm.trace()*1.0 / test_y.size
-    pr_e = ((cm.sum(axis=0)*1.0/test_y.size) * \
-            (cm.sum(axis=1)*1.0/test_y.size)).sum()
-    k_sae.append( (pr_a - pr_e) / (1 - pr_e) )
+    pr_a = cm.trace()*1.0 // test_y.size
+    pr_e = ((cm.sum(axis=0)*1.0//test_y.size) * \
+            (cm.sum(axis=1)*1.0//test_y.size)).sum()
+    k_sae.append( (pr_a - pr_e) // (1 - pr_e) )
 
     pred_y = svm_classifier.predict(test_x)
     cm = confusion_matrix(test_y, pred_y)
-    pr_a = cm.trace()*1.0 / test_y.size
-    pr_e = ((cm.sum(axis=0)*1.0/test_y.size) * \
-            (cm.sum(axis=1)*1.0/test_y.size)).sum()
-    k_svm.append( (pr_a - pr_e) / (1 - pr_e) )
+    pr_a = cm.trace()*1.0 // test_y.size
+    pr_e = ((cm.sum(axis=0)*1.0//test_y.size) * \
+            (cm.sum(axis=1)*1.0//test_y.size)).sum()
+    k_svm.append( (pr_a - pr_e) // (1 - pr_e) )
 
 std_k_sae = numpy.std(k_sae)
 std_k_svm = numpy.std(k_svm)
@@ -253,11 +255,11 @@ left =    ( (mean_k_sae - mean_k_svm) * numpy.sqrt(num_test*2-2)) \
 rv = t(num_test*2.0 - 2)
 right = rv.ppf(0.95)
 
-print '\tstd\t\tmean'
-print 'k_sae\t%f\t%f' % (std_k_sae, mean_k_sae)
-print 'k_svm\t%f\t%f' % (std_k_svm, mean_k_svm)
+print('\tstd\t\tmean')
+print('k_sae\t%f\t%f' % (std_k_sae, mean_k_sae))
+print('k_svm\t%f\t%f' % (std_k_svm, mean_k_svm))
 if left > right:
-    print 'left = %f, right = %f, test PASSED.' % (left, right)
+    print('left = %f, right = %f, test PASSED.' % (left, right))
 else:
-    print 'left = %f, right = %f, test FAILED.' % (left, right)
+    print('left = %f, right = %f, test FAILED.' % (left, right))
 
